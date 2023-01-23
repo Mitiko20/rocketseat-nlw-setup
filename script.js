dac8e1a0a116c23a1ac0a23af18fd742d5e1cd39
Leticia <let.kobayashi@gmail.com>
2023-01-22 22:18:53 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("#form-habits")
+
+const nlwSetup = new NLWSetup(form)
+
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+
+function add() {
+  const today = "01/01"
+
+  const dayExists = nlwSetup.dayExists(today)
+  
+  if(dayExists){
+    alert("Dia já incluso 🔴")
+    return
+  }
+
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
+}
+
+//const data = {
+//  run: ["01-01", "01-02", "01-06", "01-07", "01-08", "01-09"],
+//  journal: ["01-02"],
+//  takePills: ["01-06", "01-07"],
+//}
+
+nlwSetup.setData(data)
+nlwSetup.load()
