3b6aaf069cb43db80916500964b10ec51bc7d00f
Leticia <let.kobayashi@gmail.com>
2023-01-22 22:21:46 -0300

    Primeiro commit

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const form = document.querySelector("#form-habits")
-
 const nlwSetup = new NLWSetup(form)
-
 const button = document.querySelector("header button")
 
 button.addEventListener("click", add)
+form.addEventListener("change", save)
 
 function add() {
-  const today = "01/01"
-
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExists = nlwSetup.dayExists(today)
-  
-  if(dayExists){
+
+  if (dayExists) {
     alert("Dia já incluso 🔴")
     return
   }
@@ -20,11 +18,10 @@ function add() {
   nlwSetup.addDay(today)
 }
 
-//const data = {
-//  run: ["01-01", "01-02", "01-06", "01-07", "01-08", "01-09"],
-//  journal: ["01-02"],
-//  takePills: ["01-06", "01-07"],
-//}
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
 
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
